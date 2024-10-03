f6feef781da745d61b345522e4f086bb5548e69d
jedsenson <jedricclairs@gmail.com>
2024-10-03 10:42:06 +0800

    Add files via upload

---
+++ 03Variabledeclaration.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	string nickname;
+	int age;
+	
+	cout << " Enter Your name\t:\t";
+	cin >> nickname;
+	cout << " Enter Your age \t:\t";
+	cin >> age;
+	cout << " Hi " << nickname << " your age is " <<  age  << " THANK YOU!";
+	return 0;
+}
