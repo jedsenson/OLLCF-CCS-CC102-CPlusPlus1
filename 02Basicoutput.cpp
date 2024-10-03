aba4d7fdeeedf8106a2d20588ebf21e485e30c2a
jedsenson <jedricclairs@gmail.com>
2024-10-03 10:25:29 +0800

    Add files via upload

---
+++ 02Basicoutput.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	cout << " Our Lady of Loudres College Foundation " << endl;
+	cout << " BSIT 1-B " << endl;
+	cout << " Jedric Clair R. Senson " << endl;
+	cout << " Matnog Basud, Camarines norte " << endl;
+	return 0;
+}
